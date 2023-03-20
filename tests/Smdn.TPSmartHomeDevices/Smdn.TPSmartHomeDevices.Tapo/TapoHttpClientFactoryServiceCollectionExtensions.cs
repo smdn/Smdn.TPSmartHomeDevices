a7e96f3bfcf7502a961e82049f6e198eb29c466e
smdn <smdn@smdn.jp>
2023-03-21 00:35:23 +0900

    add tests for TapoHttpClientFactoryServiceCollectionExtensions

---
+++ tests/Smdn.TPSmartHomeDevices/Smdn.TPSmartHomeDevices.Tapo/TapoHttpClientFactoryServiceCollectionExtensions.cs
@@ -0,0 +1,85 @@
+// SPDX-FileCopyrightText: 2023 smdn <smdn@smdn.jp>
+// SPDX-License-Identifier: MIT
+using System;
+using System.Net.Http;
+using Microsoft.Extensions.DependencyInjection;
+using NUnit.Framework;
+
+namespace Smdn.TPSmartHomeDevices.Tapo;
+
+[TestFixture]
+public class TapoHttpClientFactoryServiceCollectionExtensionsTests {
+  [Test]
+  public void AddTapoHttpClient()
+  {
+    var services = new ServiceCollection();
+
+    services.AddTapoHttpClient();
+
+    IHttpClientFactory? factory = null;
+
+    Assert.DoesNotThrow(
+      () => factory = services.BuildServiceProvider().GetRequiredService<IHttpClientFactory>()
+    );
+    Assert.IsNotNull(factory, nameof(factory));
+  }
+
+  [Test]
+  public void AddTapoHttpClient_ConfigureClient()
+  {
+    var services = new ServiceCollection();
+    var timeout = TimeSpan.FromSeconds(1);
+    var baseAddress = new Uri("http://test.invalid/");
+
+    services.AddTapoHttpClient(
+      configureClient: client => {
+        client.Timeout = timeout;
+        client.BaseAddress = baseAddress;
+      }
+    );
+
+    IHttpClientFactory? factory = null;
+
+    Assert.DoesNotThrow(
+      () => factory = services.BuildServiceProvider().GetRequiredService<IHttpClientFactory>()
+    );
+    Assert.IsNotNull(factory, nameof(factory));
+
+    using var client = factory.CreateClient(name: string.Empty);
+
+    Assert.IsNotNull(client, nameof(client));
+    Assert.AreEqual(timeout, client.Timeout, nameof(client.Timeout));
+    Assert.AreEqual(baseAddress, client.BaseAddress, nameof(client.BaseAddress));
+  }
+
+  [Test]
+  public void AddTapoHttpClient_TryAddMultiple()
+  {
+    var services = new ServiceCollection();
+    var timeout = TimeSpan.FromSeconds(1);
+    var baseAddress = new Uri("http://test.invalid/");
+
+    services.AddTapoHttpClient(
+      configureClient: client => {
+        client.Timeout = timeout;
+        client.BaseAddress = baseAddress;
+      }
+    );
+    services.AddTapoHttpClient(
+      configureClient: static _ => throw new InvalidOperationException("this must not be invoked")
+    );
+
+    IHttpClientFactory? factory = null;
+
+    Assert.DoesNotThrow(
+      () => factory = services.BuildServiceProvider().GetRequiredService<IHttpClientFactory>()
+    );
+    Assert.IsNotNull(factory, nameof(factory));
+
+    using var client = factory.CreateClient(name: string.Empty);
+
+    Assert.IsNotNull(client, nameof(client));
+    Assert.AreEqual(timeout, client.Timeout, nameof(client.Timeout));
+    Assert.AreEqual(baseAddress, client.BaseAddress, nameof(client.BaseAddress));
+  }
+}
