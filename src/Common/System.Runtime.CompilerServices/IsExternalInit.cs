4e7bf47c9c8689da7bdb861667c1664d0abdd64c
smdn <smdn@smdn.jp>
2023-05-14 18:47:17 +0900

    suppress warning CA1812

--- src/Common/System.Runtime.CompilerServices/IsExternalInit.cs
+++ src/Common/System.Runtime.CompilerServices/IsExternalInit.cs
@@ -1,5 +1,6 @@
 // SPDX-FileCopyrightText: 2022 smdn <smdn@smdn.jp>
 // SPDX-License-Identifier: MIT
+#pragma warning disable CA1812
 
 #if !SYSTEM_RUNTIME_COMPILERSERVICES_ISEXTERNALINIT
 namespace System.Runtime.CompilerServices;
