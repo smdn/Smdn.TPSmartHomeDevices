e994d4c9e8d3707b16fb264b4a2d0af7baf705ed
smdn <smdn@smdn.jp>
2024-01-08 14:51:17 +0900

    improve XML comment docs

--- src/Smdn.TPSmartHomeDevices.Tapo/Smdn.TPSmartHomeDevices.Tapo/TapoCredentailProviderServiceCollectionExtensions.cs
+++ src/Smdn.TPSmartHomeDevices.Tapo/Smdn.TPSmartHomeDevices.Tapo/TapoCredentailProviderServiceCollectionExtensions.cs
@@ -14,6 +14,9 @@ public static class TapoCredentailProviderServiceCollectionExtensions {
   /// Adds <see cref="ITapoCredentialProvider"/> to <see cref="IServiceCollection"/>.
   /// This overload creates <see cref="ITapoCredentialProvider"/> that holds email address and password in plaintext.
   /// </summary>
+  /// <remarks>
+  /// The <see cref="ITapoCredentialProvider"/> added by this overload can be used for both <see cref="Protocol.TapoSessionProtocol.SecurePassThrough"/> and <see cref="Protocol.TapoSessionProtocol.Klap"/> authentication process.
+  /// </remarks>
   /// <param name="services">The <see cref="IServiceCollection"/> to add services to.</param>
   /// <param name="email">A plaintext email address used for authentication to the Tapo device.</param>
   /// <param name="password">A plaintext password used for authentication to the Tapo device.</param>
@@ -40,6 +43,9 @@ public static class TapoCredentailProviderServiceCollectionExtensions {
   /// Adds <see cref="ITapoCredentialProvider"/> to <see cref="IServiceCollection"/>.
   /// This overload creates <see cref="ITapoCredentialProvider"/> that holds user name (email address) and password in base64 encoded text.
   /// </summary>
+  /// <remarks>
+  /// The <see cref="ITapoCredentialProvider"/> added by this overload can only be used for <see cref="Protocol.TapoSessionProtocol.SecurePassThrough"/> authentication process.
+  /// </remarks>
   /// <param name="services">The <see cref="IServiceCollection"/> to add services to.</param>
   /// <param name="base64UserNameSHA1Digest">A base64 encoded SHA1 digest of user name (email address) text, used for authentication to the Tapo device.</param>
   /// <param name="base64Password">A base64 encoded password text, used for authentication to the Tapo device.</param>
@@ -68,6 +74,9 @@ public static class TapoCredentailProviderServiceCollectionExtensions {
   /// Adds <see cref="ITapoCredentialProvider"/> to <see cref="IServiceCollection"/>.
   /// This overload creates <see cref="ITapoCredentialProvider"/> that retrieve user name (email address) and password from environment variables.
   /// </summary>
+  /// <remarks>
+  /// The <see cref="ITapoCredentialProvider"/> added by this overload can be used for both <see cref="Protocol.TapoSessionProtocol.SecurePassThrough"/> and <see cref="Protocol.TapoSessionProtocol.Klap"/> authentication process.
+  /// </remarks>
   /// <param name="services">The <see cref="IServiceCollection"/> to add services to.</param>
   /// <param name="envVarUsername">The name of environment variable that holds the user name (email address) for authentication to the Tapo device.</param>
   /// <param name="envVarPassword">The name of environment variable that holds the password for authentication to the Tapo device.</param>
@@ -98,6 +107,9 @@ public static class TapoCredentailProviderServiceCollectionExtensions {
   /// This overload creates <see cref="ITapoCredentialProvider"/> that retrieves the base64-encoded KLAP <c>local_auth_hash</c> from environment variables.
   /// To authenticate using this method, the device firmware must support KLAP.
   /// </summary>
+  /// <remarks>
+  /// The <see cref="ITapoCredentialProvider"/> added by this overload can only be used for <see cref="Protocol.TapoSessionProtocol.Klap"/> authentication process.
+  /// </remarks>
   /// <param name="services">The <see cref="IServiceCollection"/> to add services to.</param>
   /// <param name="envVarBase64KlapLocalAuthHash">The name of environment variable that holds the <c>local_auth_hash</c> for authentication to the Tapo device.</param>
   public static IServiceCollection AddTapoBase64EncodedKlapCredentialFromEnvironmentVariable(
