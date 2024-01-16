e25070560bcecd823f60306d9145cb1834f11cd6
smdn <smdn@smdn.jp>
2024-01-16 23:51:13 +0900

    update examples for ITapoCredentialProvider

--- examples/Smdn.TPSmartHomeDevices.Tapo/Credentials/Program.cs
+++ examples/Smdn.TPSmartHomeDevices.Tapo/Credentials/Program.cs
@@ -1,7 +1,9 @@
 ﻿// SPDX-FileCopyrightText: 2023 smdn <smdn@smdn.jp>
 // SPDX-License-Identifier: MIT
 using System.Net;
+
 using Microsoft.Extensions.DependencyInjection;
+
 using Smdn.TPSmartHomeDevices.Tapo;
 using Smdn.TPSmartHomeDevices.Tapo.Credentials;
 
@@ -9,16 +11,24 @@ using Smdn.TPSmartHomeDevices.Tapo.Credentials;
 // email address and password used for authentication in the constructor.
 using var plug0 = new P105(IPAddress.Parse("192.0.2.0"), "user@mail.test", "password");
 
+
+
 // In addition to this, IServiceProvider can be used to specify the
 // credentials to be used for authentication.
 var services1 = new ServiceCollection();
 
 services1.AddTapoCredential("user@mail.test", "password");
 
+// In this example, plug1 obtains credentials from the IServiceProvider.
 using var plug1 = new P105(IPAddress.Parse("192.0.2.1"), services1.BuildServiceProvider());
+
+
 
 // You can also specify BASE64-encoded credentials instead of
 // plaintext credentials.
+//
+// But the credentials added by this method can be used only for
+// old authentication protocol ('secure pass through').
 var services2 = new ServiceCollection();
 
 services2.AddTapoBase64EncodedCredential(
@@ -27,6 +37,22 @@ services2.AddTapoBase64EncodedCredential(
 );
 
 using var plug2 = new P105(IPAddress.Parse("192.0.2.2"), services2.BuildServiceProvider());
+
+// Note:
+//   As you can see from the examples above, the authentication in Tapo's
+//   old communication protocol uses a BASE64-encoded string for the password.
+//
+//   This means that passwords can be *easily* decoded by BASE64 decoding.
+//   Therefore, consider the risks when writing passwords in your code.
+//
+//   For more secure credentials management, it is recommended to use
+//   AddTapoBase64EncodedClapCredentialFromEnvironmentVariable().
+//   See the 'CredentialsEnvVar' example for usage.
+//
+//   You can also create custom providers by implementing the
+//   ITapoCredentialProvider interface.
+
+
 
 // You can generate BASE64-encoded credentials using the TapoCredentials class.
 //
@@ -41,12 +67,3 @@ services3.AddTapoBase64EncodedCredential(
 
 using var plug3 = new P105(IPAddress.Parse("192.0.2.3"), services2.BuildServiceProvider());
 
-// Note:
-//   As you can see from the examples above, the authentication in Tapo's
-//   communication protocol uses a BASE64-encoded string for the password.
-//
-//   This means that passwords can be *easily* decoded by BASE64 decoding.
-//   Therefore, consider the risks when writing passwords in your code.
-//
-//   If you want to provide credentials by other methods for security,
-//   you can implement and use the ITapoCredentialProvider interface.
---
+++ examples/Smdn.TPSmartHomeDevices.Tapo/CredentialsEnvVar/Program.cs
@@ -0,0 +1,77 @@
+﻿// SPDX-FileCopyrightText: 2024 smdn <smdn@smdn.jp>
+// SPDX-License-Identifier: MIT
+using System.Net;
+
+using Microsoft.Extensions.DependencyInjection;
+
+using Smdn.TPSmartHomeDevices.Tapo;
+using Smdn.TPSmartHomeDevices.Tapo.Credentials;
+
+// Smdn.TPSmartHomeDevices.Tapo provides the service to retrieve
+// credentials from environment variables via IServiceProvider.
+//
+// When adding an environment variable using with the
+// AddTapoCredentialFromEnvironmentVariable() method, specify the name of
+// the environment variable that holds the user name (email address of the
+// Tapo account) and password used for authentication.
+//
+// The credentials added by this method can be used for both KLAP and
+// 'secure pass through' authentication protocols.
+var services1 = new ServiceCollection();
+
+services1.AddTapoCredentialFromEnvironmentVariable(
+  envVarUsername: "TAPO_USERNAME",
+  envVarPassword: "TAPO_PASSWORD"
+);
+
+// In this example, plug1 obtains credentials from the environment
+// variables TAPO_USERNAME and TAPO_PASSWORD.
+using var plug1 = new P105(IPAddress.Parse("192.0.2.1"), services1.BuildServiceProvider());
+
+
+
+// The above method requires that the user name and password be held
+// in the environment variables in plain text.
+//
+// The AddTapoBase64EncodedKlapCredentialFromEnvironmentVariable() method
+// can be used to hold a precomputed hash encoded in BASE64 instead of
+// plain text.
+//
+// The credentials added by this method can be used only for KLAP
+// authentication protocol.
+var services2 = new ServiceCollection();
+
+services2.AddTapoBase64EncodedKlapCredentialFromEnvironmentVariable(
+  envVarBase64KlapLocalAuthHash: "TAPO_KLAP_LOCALAUTHHASH"
+);
+
+// In this example, plug2 obtains credentials from the environment
+// variables TAPO_KLAP_LOCALAUTHHASH.
+using var plug2 = new P105(IPAddress.Parse("192.0.2.2"), services2.BuildServiceProvider());
+
+
+
+// The KLAP local auth hash can be computed by the following procedure.
+//
+//   KLAP local auth hash = SHA256(SHA1(username) + SHA1(password))
+//
+// To add to the environment variable, encode the resulting hash value
+// with BASE64 encoding.
+//
+//   KLAP_LOCALAUTHHASH = BASE64(SHA256(SHA1(username) + SHA1(password)))
+//
+// Smdn.TPSmartHomeDevices.Tapo provides the TapoCredentials.TryComputeKlapLocalAuthHash()
+// method, which can be used to calculate hash values.
+var klapLocalAuthHash = new byte[32];
+
+_ = TapoCredentials.TryComputeKlapLocalAuthHash(
+  username: "user@mail.test"u8,
+  password: "password"u8,
+  destination: klapLocalAuthHash.AsSpan(),
+  out var bytesWritten
+);
+
+Environment.SetEnvironmentVariable(
+  "TAPO_KLAP_LOCALAUTHHASH",
+  Convert.ToBase64String(klapLocalAuthHash)
+);
